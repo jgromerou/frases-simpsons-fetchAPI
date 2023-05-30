d0796a9f73224a5c5dd13e7b4fb9eb207898423e
Juan Gerardo Romero Uro <jgromerouro@gmail.com>
2023-05-30 08:02:12 -0300

    consultar a una API con la función fetch

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './assets/logosimpson.png';
+import { Button, Container } from 'react-bootstrap';
+import Frase from './components/Frase';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [personaje, setPersonaje] = useState({});
+
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  const consultarAPI = async () => {
+    //vamos hacer una peticion GET
+    const respuesta = await fetch(
+      'https://thesimpsonsquoteapi.glitch.me/quotes'
+    );
+    const datos = await respuesta.json();
+    setPersonaje(datos[0]);
+  };
+
+  return (
+    <>
+      <Container className="text-center my-5">
+        <img src={logo} alt="Logo de los simpson" className="w-50" />
+        <Frase personaje={personaje} />
+        <Button variant="warning" onClick={consultarAPI}>
+          Obtener frase
+        </Button>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Frase.jsx
@@ -0,0 +1,25 @@
+import { Card } from 'react-bootstrap';
+
+const Frase = ({ personaje }) => {
+  return (
+    <section className="my-5">
+      <h2>{personaje.character}</h2>
+      <hr />
+      <img className="mb-3" src={personaje.image} alt="" />
+      <Card>
+        <Card.Body>
+          <figure>
+            <blockquote className="blockquote">
+              <p>{personaje.quote}</p>
+            </blockquote>
+            <figcaption className="blockquote-footer">
+              personaje x in <cite title="Source Title">The simpson</cite>
+            </figcaption>
+          </figure>
+        </Card.Body>
+      </Card>
+    </section>
+  );
+};
+
+export default Frase;
